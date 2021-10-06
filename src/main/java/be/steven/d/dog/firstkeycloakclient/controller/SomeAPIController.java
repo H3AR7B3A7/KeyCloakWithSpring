d440ed6ccd8db814f669169205320c0ea6e0abab
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-10-06 22:54:35 +0200

    :sparkles: More rest controllers

--- src/main/java/be/steven/d/dog/firstkeycloakclient/controller/SomeAPIController.java
+++ src/main/java/be/steven/d/dog/firstkeycloakclient/controller/SomeAPIController.java
@@ -5,11 +5,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class SomeAPIController {
 
-    @GetMapping("/data")
+    @GetMapping("/map")
     public @ResponseBody Model getSomeObject() {
-        return new Model(1, "some object");
+        return new Model(1, "map");
+    }
+
+    @GetMapping("/tools")
+    public @ResponseBody
+    List<Model> getTools() {
+        return List.of(new Model(1, "pickaxe"), new Model(2, "shovel"), new Model(3, "torch"));
+    }
+    
+    @GetMapping("/swords")
+    public @ResponseBody
+    List<Model> getSwords() {
+        return List.of(new Model(1, "1H sword"), new Model(2, "2H sword"));
     }
 }
