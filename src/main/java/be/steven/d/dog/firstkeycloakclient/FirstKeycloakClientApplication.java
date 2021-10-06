492bf59aac75f22d31e48f4500935d4e76a6101d
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-10-06 12:38:45 +0200

    :rocket: Initial commit

---
+++ src/main/java/be/steven/d/dog/firstkeycloakclient/FirstKeycloakClientApplication.java
@@ -0,0 +1,14 @@
+package be.steven.d.dog.firstkeycloakclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstKeycloakClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstKeycloakClientApplication.class, args);
+        System.out.println("test");
+    }
+
+}
