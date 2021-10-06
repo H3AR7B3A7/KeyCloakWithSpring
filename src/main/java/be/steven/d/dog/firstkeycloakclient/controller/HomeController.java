515c75fc9f19c17eb717bd97fa509ee4595a6e20
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-10-06 16:38:44 +0200

    :sparkles: Simple authentication & Logout

---
+++ src/main/java/be/steven/d/dog/firstkeycloakclient/controller/HomeController.java
@@ -0,0 +1,12 @@
+package be.steven.d.dog.firstkeycloakclient.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+}
