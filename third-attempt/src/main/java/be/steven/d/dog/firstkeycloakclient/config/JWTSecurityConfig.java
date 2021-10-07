05f621a2ef1d14ab927675bc42436b69c57b6a6d
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-10-07 23:46:49 +0200

    :rocket: Initial commit

---
+++ third-attempt/src/main/java/be/steven/d/dog/firstkeycloakclient/config/JWTSecurityConfig.java
@@ -0,0 +1,23 @@
+package be.steven.d.dog.firstkeycloakclient.config;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class JWTSecurityConfig extends WebSecurityConfigurerAdapter {
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+    }
+}
